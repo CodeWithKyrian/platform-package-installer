843e7d5641a4a277b836cc6d2ca07ae98dc813ae
Kyrian Obikwelu <koshnawaza@gmail.com>
2026-03-31 13:17:09 +0100

    chore: Update PHP version matrix in CI workflow and fix Application import in tests

--- tests/GenerateUrlCommandTest.php
+++ tests/GenerateUrlCommandTest.php
@@ -3,9 +3,9 @@
 declare(strict_types=1);
 
 use Codewithkyrian\PlatformPackageInstaller\GenerateUrlCommand;
-use Composer\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Application;
 
 beforeEach(function () {
     $application = new Application();
